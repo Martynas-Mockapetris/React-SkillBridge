a4499256b510ac899ae1014a98c6d0c20ed3928a
Martynas-Mockapetris <marmocka@gmail.com>
2024-12-19 13:40:30 +0200

    Add basic routing and navigation components

--- frontned/src/App.jsx
+++ frontned/src/App.jsx
@@ -1,9 +1,29 @@
-function App() {
+import { Routes, Route } from 'react-router-dom';
+// Bendriniai
+import Navigation from './components/shared/Navigation';
 
+// Puslapiai
+import Home from './pages/Home';
+import Listings from './pages/Listings';
+import Profile from './pages/Profile';
+import Admin from './pages/Admin';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+function App() {
   return (
     <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/listings" element={<Listings />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontned/src/pages/Listings.jsx
@@ -0,0 +1,9 @@
+const Listings = () => {
+  return (
+    <div>
+      <h1>Freelancer Listings</h1>
+    </div>
+  );
+};
+
+export default Listings;
