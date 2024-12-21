79a654eba67b869ecec85f89e7b045b4fabb4504
Martynas-Mockapetris <marmocka@gmail.com>
2024-12-21 16:49:08 +0200

    sec: Fixed overflow issues

--- frontned/src/components/Home/PricingSection.jsx
+++ frontned/src/components/Home/PricingSection.jsx
@@ -1,7 +1,7 @@
 const PricingSection = () => {
   return (
     <section className='w-full py-20 bg-primary overflow-hidden'>
-      <div className='container mx-auto px-4 relative z-10'>
+      <div className='container mx-auto px-4 relative z-[1]'>
         <div className='text-center mb-16'>
           <h2 className='text-4xl font-heading font-bold mb-4'>
             <span className='text-light'>Flexible</span>
--- frontned/src/components/Home/TestimonialsSection.jsx
+++ frontned/src/components/Home/TestimonialsSection.jsx
@@ -102,24 +102,26 @@ const TestimonialsSection = () => {
   }
 
   return (
-    <section className='w-full py-20 bg-primary overflow-hidden relative'>
-      {/* Vidutiniai patternai */}
-      <div className='absolute -left-20 top-40 opacity-20'>
-        <img src={molecularPattern} alt='' className='w-[400px] h-[400px] rotate-[45deg]' />
-      </div>
-      <div className='absolute -right-20 bottom-20 opacity-15'>
-        <img src={molecularPattern} alt='' className='w-[400px] h-[400px] rotate-[-30deg]' />
-      </div>
+    <section className='w-full py-20 bg-primary relative'>
+      <div className='absolute inset-0 overflow-hidden'>
+        {/* Vidutiniai patternai */}
+        <div className='absolute -left-20 top-40 opacity-20'>
+          <img src={molecularPattern} alt='' className='w-[400px] h-[400px] rotate-[45deg]' />
+        </div>
+        <div className='absolute -right-20 bottom-20 opacity-15'>
+          <img src={molecularPattern} alt='' className='w-[400px] h-[400px] rotate-[-30deg]' />
+        </div>
 
-      {/* Mazi */}
-      <div className='absolute left-1/4 top-10 opacity-10'>
-        <img src={molecularPattern} alt='' className='w-[200px] h-[200px] rotate-[15deg]' />
-      </div>
-      <div className='absolute right-1/3 top-1/3 opacity-5'>
-        <img src={molecularPattern} alt='' className='w-[200px] h-[200px] rotate-[60deg]' />
-      </div>
-      <div className='absolute left-1/3 bottom-40 opacity-5'>
-        <img src={molecularPattern} alt='' className='w-[200px] h-[200px] rotate-[-15deg]' />
+        {/* Mazi */}
+        <div className='absolute left-1/4 top-10 opacity-10'>
+          <img src={molecularPattern} alt='' className='w-[200px] h-[200px] rotate-[15deg]' />
+        </div>
+        <div className='absolute right-1/3 top-1/3 opacity-5'>
+          <img src={molecularPattern} alt='' className='w-[200px] h-[200px] rotate-[60deg]' />
+        </div>
+        <div className='absolute left-1/3 bottom-40 opacity-5'>
+          <img src={molecularPattern} alt='' className='w-[200px] h-[200px] rotate-[-15deg]' />
+        </div>
       </div>
 
       <div className='container mx-auto px-4 relative z-10'>
