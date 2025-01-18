e974de671b19dd2ad6090268df6b3818584b8ad5
Martynas-Mockapetris <marmocka@gmail.com>
2025-01-18 11:54:23 +0200

    fix(admin): Add top padding to account for fixed navbar

--- frontned/src/pages/Admin.jsx
+++ frontned/src/pages/Admin.jsx
@@ -9,7 +9,7 @@ const Admin = () => {
   const [activeSection, setActiveSection] = useState('dashboard')
 
   return (
-    <div className="flex h-screen bg-gray-100 dark:bg-gray-900">
+    <div className="flex h-screen bg-gray-100 dark:bg-gray-900 pt-[100px]">
       <AdminSidebar activeSection={activeSection} setActiveSection={setActiveSection} />
       <div className="flex-1 flex flex-col overflow-hidden">
         <AdminHeader />
