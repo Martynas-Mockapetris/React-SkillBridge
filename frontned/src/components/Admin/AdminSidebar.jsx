c11e07588a038feb543931a33226a2c09a1a03a8
Martynas-Mockapetris <marmocka@gmail.com>
2025-01-18 11:31:46 +0200

    feat(admin): Add basic AdminSidebar component structure

---
+++ frontned/src/components/Admin/AdminSidebar.jsx
@@ -0,0 +1,9 @@
+const AdminSidebar = ({ activeSection, setActiveSection }) => {
+  return (
+    <div className='h-screen w-64'>
+      <div className='flex h-full flex-col'>{/* Content will go here */}</div>
+    </div>
+  )
+}
+
+export default AdminSidebar
