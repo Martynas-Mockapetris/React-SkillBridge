31d226094ba84ffd78bc760bfaf31b0bbefe5cf4
Martynas-Mockapetris <marmocka@gmail.com>
2025-02-27 16:23:47 +0200

    feat: Create authentication controller for registration and login

---
+++ server/src/controllers/authController.js
@@ -0,0 +1,67 @@
+import User from '../models/User.js'
+import { generateToken } from '../utils/jwtUtils.js'
+
+// @desc    Register a new user
+// @route   POST /api/auth/register
+// @access  Public
+export const registerUser = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password, userType } = req.body
+
+    // Check if user exists
+    const userExists = await User.findOne({ email })
+    if (userExists) {
+      return res.status(400).json({ message: 'User already exists' })
+    }
+
+    // Create user
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password,
+      userType
+    })
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        userType: user.userType,
+        token: generateToken(user._id)
+      })
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+// @desc    Login user
+// @route   POST /api/auth/login
+// @access  Public
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    // Find user by email
+    const user = await User.findOne({ email })
+    
+    // Check if user exists and password matches
+    if (user && (await user.comparePassword(password))) {
+      res.json({
+        _id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        userType: user.userType,
+        token: generateToken(user._id)
+      })
+    } else {
+      res.status(401).json({ message: 'Invalid email or password' })
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
